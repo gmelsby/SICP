d2a848e7844c48e79fc7c6771f5007b7851e65cd
Gregory Melsby <greg27426@gmail.com>
2024-06-18 22:35:23 -0400

    fixed typo in test description

--- unit-tests/1-7-tests.rkt
+++ unit-tests/1-7-tests.rkt
@@ -3,7 +3,7 @@
 (#%require rackunit "../exercises/1-7.rkt")
 
 (check-equal? (if (< (abs (- 2 (sqrt 4))) .0001) #t #f) #t "square root of 4")
-(check-equal? (if (< (abs (- 22.3607 (sqrt 500))) .0001) #t #f) #t "square root of 50")
+(check-equal? (if (< (abs (- 22.3607 (sqrt 500))) .0001) #t #f) #t "square root of 500")
 (check-equal? (if (< (abs (- 0.001 (sqrt 0.000001))) .0001) #t #f) #t "square root of 0.000001")
 (check-equal? (if (< (abs (- 0 (sqrt 0))) .0001) #t #f) #t "square root of 0")
 (check-equal? (sqrt -2) #f "square root of negative is invalid")
