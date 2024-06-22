6b0eea03bc82c4827808f2d81ec6cf39c251df24
Gregory Melsby <greg@gregexp.com>
2024-06-22 14:44:33 -0400

    created procedure for 1-12 pascal's triangle

---
+++ exercises/1-12.rkt
@@ -0,0 +1,10 @@
+#lang sicp 
+
+; calculates the element at row, col of pascal's triangle
+; top of triangle is 0, 0
+(define (pascals row col) 
+  (if (or (= col 0) (= row col))
+    1
+    (+ (pascals (- row 1) (- col 1)) (pascals (- row 1) col))
+  )
+)
