33d56f3bf4d55d7adb08df27d3609eff1a50992d
Gregory Melsby <greg@gregexp.com>
2024-06-22 12:02:33 -0400

    created iterative-process for 1-11

--- exercises/1-11.rkt
+++ exercises/1-11.rkt
@@ -11,4 +11,25 @@
   )
 )
 
-(#%provide recursive-process)
+(define (iterative-process n)
+  (iter-helper n 0 0 0 0) 
+)
+
+(define (iter-helper n current fn-minus-one fn-minus-two fn-minus-three)
+  (if (< current 3)
+
+    (if (= n current)
+      n
+      (iter-helper n (+ current 1) current fn-minus-one fn-minus-two)
+    )
+
+    (let ((fn (+ fn-minus-one (* 2 fn-minus-two) (* 3 fn-minus-three))))
+      (if (= n current)
+        fn
+        (iter-helper n (+ current 1) fn fn-minus-one fn-minus-two)
+      )
+    )
+  )
+)
+
+(#%provide recursive-process iterative-process)
--- unit-tests/1-11-tests.rkt
+++ unit-tests/1-11-tests.rkt
@@ -5,3 +5,7 @@
 (check-equal? (recursive-process 2) 2 "recursive process: n = 2")
 (check-equal? (recursive-process 3) 4 "recursive process: n = 3")
 (check-equal? (recursive-process 4) 11 "recursive process: n = 4")
+(check-equal? (iterative-process 2) 2 "iterative process: n = 2")
+(check-equal? (iterative-process 3) 4 "iterative process: n = 3")
+(check-equal? (iterative-process 4) 11 "iterative process: n = 4")
+(check-equal? (iterative-process 10) (recursive-process 10) "iterative and recursive process: n = 10")
