cf84f71a544c5c3a1f3a6911569b810a54e200c7
Gregory Melsby <greg27426@gmail.com>
2024-06-22 15:21:12 -0400

    1-16 iterative fast exponentiation

---
+++ exercises/1-16.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+(define (iterative-fast-exp base exponent) 
+  (exp-iter base exponent 1)
+)
+
+(define (is-even num) (= 0 (remainder num 2)))
+
+(define (square num) (* num num))
+
+(define (exp-iter b n a) 
+  (cond 
+    ((= n 0) a)
+    ((is-even n) (exp-iter (square b) (/ n 2) a))
+    (else (exp-iter b (- n 1) (* b a)))
+  )
+)
