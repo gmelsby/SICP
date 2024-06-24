abb9bcd98843b06c7933859e8dc1b33203f6d781
Gregory Melsby <greg@gregexp.com>
2024-06-23 23:01:04 -0400

    completed approximation of pi for 1-31

--- exercises/1-31.rkt
+++ exercises/1-31.rkt
@@ -10,8 +10,23 @@
   (iter a 1)
 )
 
+(define (factorial n) 
+  (product identity 1 inc n)
+)
+
+(define (pi-approx-nk k)
+  (if (= 0 (remainder k 2))
+    (/ (+ 2 k) (+ 1 k))
+    (/ (+ 1 k) (+ 2 k))
+  )
+)
+
+(define (pi-approx steps) 
+  (* 4 (product pi-approx-nk 1 inc steps))
+)
+
 (define (cube n) (* n n n))
 (define (product-cubes a b)
 (product cube a inc b))
 
-(#%provide product-cubes)
+(#%provide product-cubes factorial pi-approx)
--- unit-tests/1-31-tests.rkt
+++ unit-tests/1-31-tests.rkt
@@ -2,6 +2,17 @@
 
 (#%require rackunit "../exercises/1-31.rkt")
 
+; iterative product check
 (check-equal? (product-cubes 1 1) 1 "product of first cube is 1")
 (check-equal? (product-cubes 1 5) 1728000 "product of first 5 cubes is 1728000")
 (check-equal? (product-cubes 4 5) 8000 "sum of first 4 and 5 cubed is 8000")
+
+; factorial defined with product check
+(check-equal? (factorial 5) 120 "5! is 120")
+(check-equal? (factorial 10) 3628800 "10! is 3628800")
+(check-equal? (factorial 1) 1 "1! is 1")
+(check-equal? (factorial 0) 1 "0! is 1")
+
+(check-equal? 
+  (if (and (> 3.15 (pi-approx 1000)) (< 3.13 (pi-approx 1000))) #t #f) #t 
+"pi approx starts with 3.14")
