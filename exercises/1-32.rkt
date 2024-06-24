7c6cabec09f1792a6e3e05a457d0f84003d93ed1
Gregory Melsby <greg27426@gmail.com>
2024-06-24 07:55:30 -0400

    created recrusive procedure for accumulate

--- exercises/1-32.rkt
+++ exercises/1-32.rkt
@@ -10,9 +10,18 @@
   (iter a null-value)
 )
 
+(define (accumulate-recursive combiner null-value term a next b)
+  (if (> a b)
+    null-value
+    (combiner (term a) (accumulate-recursive combiner null-value term (next a) next b))
+  )
+)
+
 (define (sum term a next b) (accumulate + 0 term a next b))
+(define (sum-recursive term a next b) (accumulate-recursive + 0 term a next b))
 
 (define (product term a next b) (accumulate * 1 term a next b))
+(define (product-recursive term a next b) (accumulate-recursive * 1 term a next b))
 
 (define (cube n) (* n n n))
 (define (product-cubes a b)
@@ -23,4 +32,12 @@
   (sum cube a inc b)
 )
 
-(#%provide product-cubes product-cubes sum-cubes)
+(define (product-cubes-recursive a b)
+  (product-recursive cube a inc b)
+)
+
+(define (sum-cubes-recursive a b)
+  (sum-recursive cube a inc b)
+)
+
+(#%provide product-cubes product-cubes-recursive sum-cubes sum-cubes-recursive)
--- unit-tests/1-32-tests.rkt
+++ unit-tests/1-32-tests.rkt
@@ -10,3 +10,13 @@
 (check-equal? (sum-cubes 0 5) 225 "sum of first 5 cubes is 225")
 (check-equal? (sum-cubes 4 5) 189 "sum of first 4 and 5 cubed is 189")
 
+(check-equal? (product-cubes-recursive 1 1) 1 "product of first cube is 1")
+(check-equal? (product-cubes-recursive 1 5) 1728000 "product of first 5 cubes is 1728000")
+(check-equal? (product-cubes-recursive 4 5) 8000 "sum of first 4 and 5 cubed is 8000")
+
+(check-equal? (sum-cubes-recursive 0 1) 1 "sum of first cube is 1")
+(check-equal? (sum-cubes-recursive 0 5) 225 "sum of first 5 cubes is 225")
+(check-equal? (sum-cubes-recursive 4 5) 189 "sum of first 4 and 5 cubed is 189")
+
+
+
