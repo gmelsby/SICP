1f9a95a94ce72aba2b59fe99c6556b791526e37f
Gregory Melsby <greg@gregexp.com>
2024-07-03 17:47:13 -0400

    1-36 complete

---
+++ exercises/1-36.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+(define (average x y) (/ (+ x y) 2.0))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) 
+       tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (display next)
+      (newline)
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(display "without average damping")
+(newline)
+(fixed-point (lambda (x) (/ (log 1000) (log x))) 2.0)
+(newline)
+(display "with average damping")
+(newline)
+(fixed-point (lambda (x) (average x (/ (log 1000) (log x)))) 2.0)
+
