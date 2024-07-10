f2fa5b38d4f30a53762b76ccb90d37f590f65027
Gregory Melsby <greg27426@gmail.com>
2024-07-10 17:42:31 -0400

    added some examples for 1-42.rkt

--- exercises/1-42.rkt
+++ exercises/1-42.rkt
@@ -1,5 +1,13 @@
 #lang sicp
 
+(define (square x) (* x x))
+(define (cube x) (* x x x))
+
 (define (compose f g) (
   lambda (x) (f (g x))
 ))
+
+((compose square cube) 2)
+((compose cube cube) 2)
+((compose inc square) 2)
+((compose square inc) 2)
