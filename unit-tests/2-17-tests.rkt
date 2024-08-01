d05044a9d656be163c48c5bf48f13f419cb99215
Gregory Melsby <greg@gregexp.com>
2024-08-01 12:44:17 -0400

    completed 2-17

---
+++ unit-tests/2-17-tests.rkt
@@ -0,0 +1,6 @@
+#lang sicp
+
+(#%require rackunit "../exercises/2-17.rkt")
+
+(check-equal? (last-pair (list 23 72 149 34)) 34 "last pair of list is 34")
+(check-equal? (last-pair (list 2)) 2 "last pair of list of length 1 is 2")
