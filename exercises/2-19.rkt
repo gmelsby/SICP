c49b4a59bd009aa295f483711eaf173701977bfe
Gregory Melsby <greg@gregexp.com>
2024-08-03 17:49:56 -0400

    fixed 2-17

---
+++ exercises/2-19.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+(define (no-more? values)
+  (null? values)
+)
+
+(define (except-first-denomination values) 
+  (cdr values)
+)
+
+(define (first-denomination values)
+  (car values)
+)
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 
+         1)
+        ((or (< amount 0) 
+             (no-more? coin-values)) 
+         0)
+        (else
+         (+ (cc 
+             amount
+             (except-first-denomination 
+              coin-values))
+            (cc 
+             (- amount
+                (first-denomination 
+                 coin-values))
+             coin-values)))))
+
+(#%provide cc)
--- unit-tests/2-17-tests.rkt
+++ unit-tests/2-17-tests.rkt
@@ -2,5 +2,5 @@
 
 (#%require rackunit "../exercises/2-17.rkt")
 
-(check-equal? (last-pair (list 23 72 149 34)) 34 "last pair of list is 34")
-(check-equal? (last-pair (list 2)) 2 "last pair of list of length 1 is 2")
+(check-equal? (last-pair (list 23 72 149 34)) (list 34) "last pair of list is 34")
+(check-equal? (last-pair (list 2)) (list 2) "last pair of list of length 1 is 2")
