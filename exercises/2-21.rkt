072f012b2ec4ae31b65cfc62e0adfcc9a936d495
Gregory Melsby <greg@gregexp.com>
2024-08-13 16:14:23 -0400

    2-21 ready for tests

---
+++ exercises/2-21.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+
+(define (square x) (* x x))
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (square (car items)) (square-list (cdr items)))
+  )
+)
+
+(define (square-list-map items)
+  (map square items))
+
+(#%provide square-list square-list-map)
