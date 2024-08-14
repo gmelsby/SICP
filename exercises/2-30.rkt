2d125466a2b9b8f53101cbe3beab1bcf36a60583
Gregory Melsby <greg@gregexp.com>
2024-08-14 15:43:33 -0400

    created map version of square-tree

--- exercises/2-30.rkt
+++ exercises/2-30.rkt
@@ -12,4 +12,13 @@
   )
 )
 
-(#%provide square-tree)
+(define (square-tree-map ele) 
+(cond 
+    ((null? ele) '())
+    ((pair? ele)
+      (map square-tree-map ele))
+    (else (square ele))
+  )
+)
+
+(#%provide square-tree square-tree-map)
