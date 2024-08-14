e3c1127c30d78e4b9fc3d986f4833a0d8aa4a805
Gregory Melsby <greg27426@gmail.com>
2024-08-14 15:43:33 -0400

    created map version of square-tree

--- unit-tests/2-30-tests.rkt
+++ unit-tests/2-30-tests.rkt
@@ -12,3 +12,13 @@
     (list 36 49))
   "book example")
 (check-equal? (square-tree '()) '() "empty list")
+
+(check-equal? (square-tree-map
+  (list 1
+    (list 2 (list 3 4) 5)
+    (list 6 7)))
+  (list 1
+    (list 4 (list 9 16) 25)
+    (list 36 49))
+  "book example map")
+(check-equal? (square-tree-map '()) '() "empty list map")
