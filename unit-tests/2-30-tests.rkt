073d1d3d49f8715c0515daea5159b22042ccd306
Gregory Melsby <greg27426@gmail.com>
2024-08-14 15:34:45 -0400

    2-30 tests for directly defined square-tree pass

---
+++ unit-tests/2-30-tests.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(#%require rackunit "../exercises/2-30.rkt")
+
+
+(check-equal? (square-tree
+  (list 1
+    (list 2 (list 3 4) 5)
+    (list 6 7)))
+  (list 1
+    (list 4 (list 9 16) 25)
+    (list 36 49))
+  "book example")
+(check-equal? (square-tree '()) '() "empty list")
