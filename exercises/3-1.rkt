d121d19b75cbb3d3effa5d038ae452cb107aa80c
Gregory Melsby <greg@gregexp.com>
2024-08-20 14:39:20 -0400

    3-1 complete

---
+++ exercises/3-1.rkt
@@ -0,0 +1,15 @@
+#lang sicp
+
+(define (make-accumulator val)
+  (lambda (x) 
+    (set! val (+ val x))
+    val
+  ) 
+)
+
+(define A (make-accumulator 5))
+(A 10)
+(A 0)
+(A 56)
+
+(#%provide make-accumulator)
